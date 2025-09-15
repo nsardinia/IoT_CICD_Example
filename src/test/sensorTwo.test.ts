60c77d329b23b6782f2a3ad0178bed7e61ff049f
Nicholas Sardinia <nicholas_sardinia@outlook.com>
2025-09-15 13:42:35 -0400

    fixed test values

--- src/test/sensorTwo.test.ts
+++ src/test/sensorTwo.test.ts
@@ -3,14 +3,14 @@ import { isValidSensorValue } from "../sensor";
 
 describe("isBrokenSensor()", () => {
   it("returns false for values inside 0 and 150", () => {
-    expect(isValidSensorValue(50)).toBe(false);
+    expect(isValidSensorValue(50)).toBe(true);
   });
 
   it("returns true for negative values", () => {
-    expect(isValidSensorValue(-1)).toBe(true);
+    expect(isValidSensorValue(-1)).toBe(false);
   });
 
   it("returns true for values above 100", () => {
-    expect(isValidSensorValue(101)).toBe(true);
+    expect(isValidSensorValue(101)).toBe(false);
   });
 });
